30ca7e283cdc8de4df0e5c3b05c3b44e894a025c
stereobit <contact@stereobit.com>
2020-06-27 13:05:39 +0300

    update 2020-2

---
+++ www7/cp/html/media-center/mob@sidemenu-wrapper.php
@@ -0,0 +1,14 @@
+<!-- ================================== MEGAMENU VERTICAL ================================== -->
+<div class="row">
+
+    <div class="col-xs-12 col-lg-6">
+        <ul>
+            $0$
+        </ul>
+    </div>
+
+    <!--div class="dropdown-banner-holder">
+		<a href="#"><img alt="banner-side-$2$" src="/images/banners/banner-side-$2$.png" /></a>
+    </div-->
+</div>
+<!-- ================================== MEGAMENU VERTICAL ================================== -->
